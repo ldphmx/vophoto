cc204a4c30ad8717c311097be3e03a7f291bb527
zouhenry001 <allenzou1990@126.com>
2015-08-26 16:33:53 +0800

    Update UploadHandler.py
    update for images that has existed,in which the user just want to extend the image-tags

--- src/UploadHandler.py
+++ src/UploadHandler.py
@@ -13,18 +13,15 @@ class UploadHandler(BaseAuthenticateHandler.BaseAuthenticateHandler):
         try:
             userId = self.get_argument('user_id', '')
             rawLocation = self.get_argument('loc','')
-            # latitude = self.get_argument('lat', '')
-            # longitude = self.get_argument('lon', '')
             desc = self.get_argument('desc', '')
             rawTags = self.get_argument('tag', '')
             rowTime = self.get_argument('time', '')
-             clientLocation = self.get_argument('clocat','')  #add by peigang
-        ######added by peigang###
+            clientLocation = self.get_argument('clocat','')  #add by peigang
             token = self.get_argument('token','')
             user = MongoHelper.get_user_by_id(userId)
             if token != user['token']:
                 return
-        ######added by peigang###
+        
             path = Utils.get_user_path(userId)
             files = self.request.files['image']
             
@@ -34,32 +31,39 @@ class UploadHandler(BaseAuthenticateHandler.BaseAuthenticateHandler):
                 return
             fileinfo = files[0]
             fname = fileinfo['filename']
+           
+            ###for images that has uploaded:if this image was existed,then the customer just want to extend image-tags###
             if check_img_exist(userId, fname):
-                self.write(json.dumps(result))
-                return
-            fh = open(path + "/" + fname, 'wb')
-            fh.write(fileinfo['body'])
+                key_words = rawTags.split(' ')
+                tags = Utils.get_meaningful_keywords(key_words)
+                MongoHelper.extend_tags_in_existimage(userId,fname,tags)
+                result['status'] = True
+            ###for images that has not uploaded###    
+            else:
+                fh = open(path + "/" + fname, 'wb')
+                fh.write(fileinfo['body'])
             
             # filter out meaningful tags
-            key_words = rawTags.split(' ')
-            if key_words is None or len(key_words) == 0:
-                return
-            tags = Utils.get_meaningful_keywords(key_words)
+                key_words = rawTags.split(' ')
+                if key_words is None or len(key_words) == 0:
+                    return
+                tags = Utils.get_meaningful_keywords(key_words)
+            
             
             # split date and time
-            time = datetime.strptime(rowTime, '%Y-%m-%d %X %z')
-            ##added by peigang
-            key_location = rawLocation.split(',')                       
-            if key_location is None or len(key_location) == 0:
-                return
-            location = Utils.get_location_from_rawlocation(key_location)
-            raw_location_tag = Utils.get_tag_from_rawlocation(key_location)
-            tags.extend(raw_location_tag)
+                time = datetime.strptime(rowTime, '%Y-%m-%d %X %z')
             
-            #update client_loc and location
-            image = {'user_id': userId, 'image_name': fname, 'client_loc': clientLocation,'location':location, 'desc': desc, 'tags': tags, 'time':time, 'processed': False}
-            MongoHelper.save_image(image)
-            result['status'] = True
+            
+                key_location = rawLocation.split(',')               #add         
+                if key_location is None or len(key_location) == 0:
+                    return
+                location = Utils.get_location_from_rawlocation(key_location)
+                raw_location_tag = Utils.get_tag_from_rawlocation(key_location)
+                tags.extend(raw_location_tag)
+            
+                image = {'user_id': userId, 'image_name': fname, 'client_loc': clientLocation,'location':location, 'desc': desc, 'tags': tags, 'time':time, 'processed': False}
+                MongoHelper.save_image(image)
+                result['status'] = True
         finally:
             self.write(json.dumps(result))
         
