ce5cfcb9e67344789988eaf486941f01524ef479
zouhenry001 <allenzou1990@126.com>
2015-08-26 16:36:04 +0800

    Update Config.py

--- src/Config.py
+++ src/Config.py
@@ -4,9 +4,9 @@ config = {
     'access_token': 'secret',
     'mongo_url':'mongodb://localhost:27017/',
     'photo_root': 'E:/data/photos/',
-    'servers': [{'name' : 'localhost', 'capacity': 100}],
+    'servers': [{'name' : 'localhost', 'capacity': 100,'count':''}],      #updated
     'image_process_batch': 10,
-    'meaningful_pos':['ns','nt','n'],
+    'meaningful_pos':['ns','n'],    #updated
     'human_name_pos':['nh'],
     'memcached_host': '127.0.0.1',
     'face_api_key':'842282b7109d49b1914b0541cb6b1ad5'
