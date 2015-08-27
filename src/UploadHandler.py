1825ecff1c8918db4b3aee53a1aa9d85a520d797
zouhenry001 <allenzou1990@126.com>
2015-08-27 15:38:23 +0800

    Update UploadHandler.py

--- src/UploadHandler.py
+++ src/UploadHandler.py
@@ -4,7 +4,7 @@ import json
 from src import BaseAuthenticateHandler
 from src import Utils
 from src import MongoHelper
-from src.MongoHelper import check_img_exist
+# from src.MongoHelper import check_img_exist
 from datetime import datetime
 
 class UploadHandler(BaseAuthenticateHandler.BaseAuthenticateHandler):   
@@ -12,42 +12,43 @@ class UploadHandler(BaseAuthenticateHandler.BaseAuthenticateHandler):
         result = {'status': False}
         try:
             userId = self.get_argument('user_id', '')
-            rawLocation = self.get_argument('loc','')
+            rawLocation = self.get_argument('loc','')   #add           
             desc = self.get_argument('desc', '')
             rawTags = self.get_argument('tag', '')
             rowTime = self.get_argument('time', '')
-            clientLocation = self.get_argument('clocat','')  #add by peigang
-            token = self.get_argument('token','')
+            clientLocation = self.get_argument('path','')  #add
+            token = self.get_argument('token','')           #add
+            image_name = self.get_argument('image','')    #add
             user = MongoHelper.get_user_by_id(userId)
-            if token != user['token']:
+            if token != user['token']:      #add
                 return
-        
-            path = Utils.get_user_path(userId)
-            files = self.request.files['image']
-            
-            # process image file
-            if files is None or len(files) == 0:
-                self.write(json.dumps(result))
-                return
-            fileinfo = files[0]
-            fname = fileinfo['filename']
-           
             ###for images that has uploaded:if this image was existed,then the customer just want to extend image-tags###
-            if check_img_exist(userId, fname):
+            if isinstance(image_name,str):
                 key_words = rawTags.split(' ')
                 tags = Utils.get_meaningful_keywords(key_words)
-                MongoHelper.extend_tags_in_existimage(userId,fname,tags)
+                MongoHelper.extend_tags_in_existimage(userId,image_name,tags)
                 result['status'] = True
-            ###for images that has not uploaded###    
-            else:
+            
+            else:    ###for images that has not uploaded###    
+                path = Utils.get_user_path(userId)
+            #if self.request.files:
+             
+                files = self.request.files['image']
+            
+            # process image file
+                if files is None or len(files) == 0:
+                    self.write(json.dumps(result))
+                    return
+                fileinfo = files[0]
+                fname = fileinfo['filename']    
                 fh = open(path + "/" + fname, 'wb')
                 fh.write(fileinfo['body'])
             
             # filter out meaningful tags
                 key_words = rawTags.split(' ')
-                if key_words is None or len(key_words) == 0:
-                    return
-                tags = Utils.get_meaningful_keywords(key_words)
+                tags = []
+                if key_words is not None and len(key_words) != 0:
+                    tags = Utils.get_meaningful_keywords(key_words)
             
             
             # split date and time
@@ -55,15 +56,15 @@ class UploadHandler(BaseAuthenticateHandler.BaseAuthenticateHandler):
             
             
                 key_location = rawLocation.split(',')               #add         
-                if key_location is None or len(key_location) == 0:
-                    return
-                location = Utils.get_location_from_rawlocation(key_location)
-                raw_location_tag = Utils.get_tag_from_rawlocation(key_location)
-                tags.extend(raw_location_tag)
+                raw_location_tag = []
+                if key_location is not None and len(key_location) != 0:
+                    location = Utils.get_location_from_rawlocation(key_location)
+                    raw_location_tag = Utils.get_tag_from_rawlocation(key_location)
+                    tags.extend(raw_location_tag)
             
-                image = {'user_id': userId, 'image_name': fname, 'client_loc': clientLocation,'location':location, 'desc': desc, 'tags': tags, 'time':time, 'processed': False}
-                MongoHelper.save_image(image)
-                result['status'] = True
+                    image = {'user_id': userId, 'image_name': fname, 'client_loc': clientLocation,'location':location, 'desc': desc, 'tags': tags, 'time':time, 'processed': False}
+                    MongoHelper.save_image(image)
+                    result['status'] = True
         finally:
             self.write(json.dumps(result))
         
