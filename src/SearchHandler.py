8b8e904afc425ee0abc8bc26fcf669aafdf1f714
zouhenry001 <allenzou1990@126.com>
2015-08-28 16:04:20 +0800

    Update SearchHandler.py

--- src/SearchHandler.py
+++ src/SearchHandler.py
@@ -20,19 +20,26 @@ class SearchHandler(BaseAuthenticateHandler.BaseAuthenticateHandler):
             token = self.get_argument('token','')
             user = MongoHelper.get_user_by_id(user_id)
             if token != user['token']:
+                self.write(json.dumps(result))
                 return
-        ######added by peigang###
+        
             if user_id == '' or rawTag == '':
+                self.write(json.dumps(result))
                 return
-        ###peigang
+       
             key_location = rawLocation.split(',')
             latitude = float(key_location[0])
             longitude = float(key_location[1])  
             image_to_client = []
             key_words = rawTag.split(' ')
             if key_words is None or len(key_words) == 0:
+                self.write(json.dumps(result))
                 return
             meaningful = Utils.get_meaningful_keywords(key_words)
+            face_id = Utils.get_faceid_from_rawTags(rawTag)
+            if face_id is not None:
+                meaningful.append(face_id)
+            
             raw_image  = Utils.get_images_by_tag(user_id, meaningful)   ##需修改这个函数：search时容错、关联度
             
             #get time_range#
@@ -43,12 +50,15 @@ class SearchHandler(BaseAuthenticateHandler.BaseAuthenticateHandler):
                 image = raw_image
             elif key_location is None and time_range is not None:   
                 image = Utils.get_image_depend_timerange(raw_image,time_range)
-            else:
+            elif key_location is not None and time_range is not None:
                 Traw_image = Utils.get_image_depend_timerange(raw_image,time_range)
-                image = Utils.get_images_by_location_from_photos(latitude, longitude,Traw_image)   
+                image = Utils.get_images_by_location_from_photos(latitude, longitude,Traw_image)
+            else:
+                image = Utils.get_images_by_location_from_photos(latitude, longitude,raw_image)   
             for item in image:
                 image_info = item[0]
                 image_to_client.append(image_info['img_name'])
+            
             result['status'] = True
             result['image'] = image_to_client
             
