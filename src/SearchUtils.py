75d899807ee3f54f5f2c371969ec61ad82ad90ff
yswhynot <yswhynot@gmail.com>
2015-08-31 11:27:09 +0800

    update imput parameter of search utils

--- src/SearchUtils.py
+++ src/SearchUtils.py
@@ -9,7 +9,7 @@ from pymemcache.client.base import Client
 from scipy import spatial
 import numpy as np
 
-def sort_by_location(image_list, location):
+def sort_by_location(longitude, latitude, image_list):
     sorted_images = []
     if not image_list:
         return None
@@ -22,12 +22,12 @@ def sort_by_location(image_list, location):
             index_images[0].append([x, y])
             index_images[1].append(image['image_name'])
 # index_images = [[[14.32, 15.32], [0.89, 0.56], [6.36, 3.66]], ['img01', 'img03', 'img04']]
-        res_image_list = sort_by_closest_point(index_images, location)
+        res_image_list = sort_by_closest_point(index_images, longitude, latitude)
         sorted_images += res_image_list
     return sorted_images
 
 
-def sort_by_closest_point(indexer, location):
+def sort_by_closest_point(indexer, longitude, latitude):
     sorted_images = []
     if not indexer:
         return None
@@ -36,9 +36,8 @@ def sort_by_closest_point(indexer, location):
     
     pts = np.array(indexer[0])
     tree = spatial.KDTree(pts)
-    loc_point = np.array([location['longitude'], location['latitude']])
+    loc_point = np.array([longitude, latitude])
     results = tree.query(loc_point, k=len(indexer[1]))  #[[distance],[index]]
-    test = results[1]
     for res_index in results[1]:
         sorted_images.append(indexer[1][res_index])
     return sorted_images
@@ -49,5 +48,5 @@ if __name__ == '__main__':
                  {'image_name': 'img03.jpg', 'location': {'longitude': 40.123456, 'latitude': 85.456789}}],
                  [{'image_name': 'img04.jpg', 'location': {'longitude': 45.123456, 'latitude': 25.456789}},
                   {'image_name': 'img05.jpg', 'location': {'longitude': 15.123456, 'latitude': 16.456789}}]]
-    sorted_imgs = sort_by_location(imageList, {'longitude': 14.123001, 'latitude': 15.456001})
+    sorted_imgs = sort_by_location(14.123001, 15.456001, imageList)
     print(sorted_imgs)
