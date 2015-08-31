836a14e7e18cf47dcd3b684d4b9b2a33d6164da9
zouhenry001 <allenzou1990@126.com>
2015-08-31 22:04:00 +0800

    Update SearchHandler.py

--- src/SearchHandler.py
+++ src/SearchHandler.py
@@ -15,7 +15,7 @@ class SearchHandler(BaseAuthenticateHandler.BaseAuthenticateHandler):
             desc = self.get_argument('desc','')   #add
             rawTag = self.get_argument('tag', '')
             # 我_r 想_v 找_v 去年_nt 夏天_nt 在_p 西雅图_ns 农贸市场_n 的_u 照片_n
-        ###peigang
+  
             rawLocation = self.get_argument('loc','')
             token = self.get_argument('token','')
             user = MongoHelper.get_user_by_id(user_id)
@@ -26,7 +26,7 @@ class SearchHandler(BaseAuthenticateHandler.BaseAuthenticateHandler):
             if user_id == '' or rawTag == '':
                 self.write(json.dumps(result))
                 return
-       
+
             key_location = rawLocation.split(',')
             latitude = float(key_location[0])
             longitude = float(key_location[1])  
@@ -36,31 +36,31 @@ class SearchHandler(BaseAuthenticateHandler.BaseAuthenticateHandler):
                 self.write(json.dumps(result))
                 return
             meaningful = Utils.get_meaningful_keywords(key_words)
-            face_id = Utils.get_faceid_from_rawTags(rawTag)
+            face_name = Utils.get_human_names(rawTag)
+            face_id = list(MongoHelper.get_similar_persons(user_id,face_name))  
             if face_id is not None:
                 meaningful.append(face_id)
             
-            raw_image  = Utils.get_images_by_tag(user_id, meaningful)   ##需修改这个函数：search时容错、关联度
-            
-            #get time_range#
             time_range = NLPTimeConvertor.time_api(rawTag)
-
-            
-            if key_location is None and time_range is None:    
-                image = raw_image
-            elif key_location is None and time_range is not None:   
-                image = Utils.get_image_depend_timerange(raw_image,time_range)
-            elif key_location is not None and time_range is not None:
-                Traw_image = Utils.get_image_depend_timerange(raw_image,time_range)
-                image = Utils.get_images_by_location_from_photos(latitude, longitude,Traw_image)
-            else:
-                image = Utils.get_images_by_location_from_photos(latitude, longitude,raw_image)   
+            if time_range is not None and key_location is not None:
+                Timage = Utils.get_image_by_time(user_id, time_range)    ##yisa
+                Tag_image = Utils.get_images_by_tag_from_Timage(user_id,meaningful,Timage)
+                image = Utils.sort_by_location(latitude,longitude,Tag_image)
+            elif time_range is not None and key_location is None:
+                Timage = Utils.get_image_depend_timerange_from_index(time_range)
+                image = Utils.get_images_by_tag_from_Timage(user_id,meaningful,Timage)
+            elif time_range is None and key_location is not None:
+                Tag_image = Utils.get_images_by_tag(user_id, meaningful)
+                image = Utils.sort_by_location(latitude,longitude,Tag_image)
+            elif time_range is None and key_location is None:
+                image = Utils.get_images_by_tag(user_id, meaningful)
+                
             for item in image:
-                image_info = item[0]
-                image_to_client.append(image_info['img_name'])
-            
+                image_to_client.append(item)
+                
             result['status'] = True
             result['image'] = image_to_client
-            
+
+
         finally:
             self.write(json.dumps(result))
