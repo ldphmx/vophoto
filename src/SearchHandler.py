25236dcde4aec530392bf5c045cde567b6db04aa
yswhynot <yswhynot@gmail.com>
2015-09-06 17:35:26 +0800

    update logger

--- src/SearchHandler.py
+++ src/SearchHandler.py
@@ -5,7 +5,7 @@ from src import Logger
 import MongoHelper
 import json
 import Utils
-import BaseAuthenticateHandler
+from src import BaseAuthenticateHandler
 import NLPTimeConvertor
 
 class SearchHandler(BaseAuthenticateHandler.BaseAuthenticateHandler):
@@ -15,30 +15,32 @@ class SearchHandler(BaseAuthenticateHandler.BaseAuthenticateHandler):
         try:
             user_id = self.get_argument('user_id', '')
             desc = self.get_argument('desc','')   #add
-            print('user_id:',user_id)
+            
             rawTag = self.get_argument('tag', '')
             # 我_r 想_v 找_v 去年_nt 夏天_nt 在_p 西雅图_ns 农贸市场_n 的_u 照片_n
-            print('rawTag:',rawTag)
             rawLocation = self.get_argument('loc','')
-            print('rawLocation:',rawLocation)
             token = self.get_argument('token','')
             user = MongoHelper.get_user_by_id(user_id)
-            print('user:',user)
+            Logger.debug('user_id: ' + user_id + ', rawTag: ' + rawTag + ', rawLocation: ' + rawLocation + ', user: ' + user)
             if token != user['token']:
                 self.write(json.dumps(result))
+                Logger.debug('user token false')
                 return
         
             if user_id == '' or rawTag == '':
                 self.write(json.dumps(result))
+                Logger.debug('user id or rawTag null')
                 return
 
             key_words = rawTag.split(' ')
-            print(key_words)
+            Logger.debug('key words: ' + key_words)
             if key_words is None or len(key_words) == 0:
                 self.write(json.dumps(result))
+                Logger.debug('key_word is none')
                 return
+            
             meaningful = Utils.get_meaningful_keywords(key_words)
-            print('meaningful:',meaningful)
+            Logger.debug('meaningful: ' + meaningful)
             if rawLocation != '': 
                 key_location = rawLocation.split(',')
                 print('key_locayion:',key_location)
@@ -49,7 +51,7 @@ class SearchHandler(BaseAuthenticateHandler.BaseAuthenticateHandler):
                 key_location_tag = Utils.get_tag_from_rawlocation(key_location)
                 print('key_location_tag:',key_location_tag)    
                 meaningful.extend(key_location_tag)
-            print('meaningful:',meaningful)
+            Logger.debug('meaningful: ' + meaningful)
 #             face_name = Utils.get_human_names(rawTag)
 #             face_id = list(MongoHelper.get_similar_persons(user_id,face_name))  
 #             if face_id :
@@ -60,7 +62,7 @@ class SearchHandler(BaseAuthenticateHandler.BaseAuthenticateHandler):
 #                 meaningful.extend(cv_tags)
             
             time_range = NLPTimeConvertor.time_api(rawTag)
-            print('time_range:',time_range)
+            Logger.debug('time_range: ' + time_range)
             if time_range and rawLocation != '':
                 Timage = Utils.get_image_by_time(user_id, time_range)    
                 Tag_image = Utils.get_images_by_tag_from_Timage(user_id,meaningful,Timage,1)
