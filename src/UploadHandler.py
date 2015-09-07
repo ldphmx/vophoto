07abcbaf48af2d27a20a0a05618b6df54833f950
zouhenry001 <allenzou1990@126.com>
2015-09-07 10:46:17 +0800

    add

--- src/UploadHandler.py
+++ src/UploadHandler.py
@@ -16,7 +16,7 @@ class UploadHandler(BaseAuthenticateHandler.BaseAuthenticateHandler):
         try:
             userId = self.get_argument('user_id', '')
             rawLocation = self.get_argument('loc','')   #add           
-            desc = self.get_argument('tag', '')
+            desc = [self.get_argument('tag', '')]
             rawTags = self.get_argument('tag', '')
             rowTime = self.get_argument('time', '')
             function = self.get_argument('func','')  #
